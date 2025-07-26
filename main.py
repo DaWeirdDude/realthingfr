d557e449ced2cbeef01092c9b326476a578040ad
DaWeirdDude <150169133+DaWeirdDude@users.noreply.github.com>
2025-07-26 10:20:14 +0400

    Update main.py

--- main.py
+++ main.py
@@ -119,9 +119,9 @@ async def on_message(message):
             "It's never lupus you absolute dumbfuck.",
             "Lupus? That's cute. Try harder.",
             "Still not lupus gang. It's never lupus.",
-            "<@510784737800093716> would be disappointed."
-            "Lupus? I had lupus once. They locked me in a room. A room filled with lupus. And lupus makes me lupus."
-            "Gang plz stop im done saying its never lupus."
+            "<@510784737800093716> would be disappointed.",
+            "Lupus? I had lupus once. They locked me in a room. A room filled with lupus. And lupus makes me lupus.",
+            "Gang plz stop im done saying its never lupus.",
         ]
         await message.channel.send(random.choice(lupus_responses))
 
